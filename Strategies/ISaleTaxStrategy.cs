edb06c1f5db8a8ea7932b7a7ac3f4a979f220f1a
muhammad.noman <muhammad.noman@bentley.com>
2020-10-28 23:43:48 +0500

    Initial code after adding strategy

---
+++ Strategies/ISaleTaxStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy_Pattern_First_Look.Strategies
+{
+    public interface ISaleTaxStrategy
+    {
+        decimal GetTax(Order order);
+    }
+}
---
+++ Strategies/SwedenSaleTaxStrategy.cs
@@ -0,0 +1,42 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy_Pattern_First_Look.Strategies
+{
+    public class SwedenSaleTaxStrategy : ISaleTaxStrategy
+    {
+        public decimal GetTax(Order order)
+        {
+        var destination = order.ShippingDetails.DestinationCountry.ToLowerInvariant();
+            if (destination == order.ShippingDetails.OriginCountry.ToLowerInvariant())
+            {
+                decimal totalTax = 0m;
+                foreach (var item in order.LineItems)
+                {
+                    switch (item.Key.ItemType)
+                    {
+                        case ItemType.Food:
+                            totalTax += (item.Key.Price * 0.06m) * item.Value;
+                            break;
+
+                        case ItemType.Literature:
+                            totalTax += (item.Key.Price * 0.08m) * item.Value;
+                            break;
+
+                        case ItemType.Service:
+                        case ItemType.Hardware:
+                            totalTax += (item.Key.Price * 0.25m) * item.Value;
+                            break;
+                    }
+                }
+
+                return totalTax;
+            }
+
+            return 0;
+
+        }
+    }
+}
